a3324458d9a934f689a416fe0f08e69b2fb9f13a
Tim Waite <tim@dialexa.com>
2022-02-04 12:00:50 -0600

    Set canvas size correctly

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas ref="canvas" />
+  <canvas ref="canvas" :height="HEIGHT" :width="WIDTH" />
 </template>
 
 <script lang="ts">
@@ -11,31 +11,36 @@ const WIDTH = 800;
 export default defineComponent({
   name: 'Canvas',
   setup() {
-    const canvas = ref<HTMLCanvasElement>(null);
+    const canvas = ref<HTMLCanvasElement | null>(null);
 
     onMounted(() => {
-      const ctx = canvas.value.getContext('2d');
-      const image = ctx.getImageData(0, 0, WIDTH, HEIGHT);
-      const data = image.data;
+      const ctx = canvas.value?.getContext('2d');
+      const image = ctx?.getImageData(0, 0, WIDTH, HEIGHT);
+      const data = image?.data;
 
-      for (var i = 0; i < data.length; i += 4) {
-        updateColor(data, i, randomInt(0, 255), randomInt(0, 255), randomInt(0, 255), 255);
+      if (data?.length && image && data) {
+
+        for (var i = 0; i < data.length; i += 4) {
+          updateColor(data, i, randomInt(0, 255), randomInt(0, 255), randomInt(0, 255), 255);
+        }
+
+        ctx?.putImageData(image, 0, 0);
       }
-
-      ctx.putImageData(image, 0, 0);
     })
 
     return {
-      canvas
+      canvas,
+      HEIGHT,
+      WIDTH
     }
   }
 });
 
-function randomInt(min, max) {
+function randomInt(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function updateColor(data, cell, r, g, b, a = 255) {
+function updateColor(data: Uint8ClampedArray, cell: number, r: number, g: number, b: number, a = 255) {
   data[cell] = r;
   data[cell+1] = g;
   data[cell+2] = b;
